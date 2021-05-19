c4682961f088a7a5e1d6571ab46b5fc2063ac1eb
Dakota O'Neil <oneilkody@gmail.com>
2021-05-19 13:00:04 -0500

    working rap api

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const PORT = 8000
+
+app.use(cors())
+
+let rappers = {
+    '21 Savage': {
+        'age': 28,
+        'birthName': 'Sheyaa Bin Abraham-Joseph',
+        'birthLocation': 'London, England'
+    },
+    'Chance The Rapper': {
+        'age': 27,
+        'birthName': 'Chancellor Jonathan Bennett', 
+        'birthLocation': 'Chicago, Illinois'
+    },
+    'dylan': {
+        'age': 28,
+        'birthName': 'dylan', 
+        'birthLocation': 'dylan'
+    }
+}
+
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/index.html')
+})
+app.get('/api/rappers/:rapperName', (request, response)=> {
+    const rapName = request.params.rapperName
+    console.log(rapName)
+    if (rappers[rapName]) {
+        response.json(rappers[rapName])
+    } else {
+        response.json(rappers['dylan'])
+    }
+    
+})
+
+app.listen(PORT, ()=> {
+    console.log(`Server running on port ${PORT}`)
+})
